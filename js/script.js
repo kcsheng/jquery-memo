2d2c8e03c50c2e55ae7427f431f63100ac196fa5
kcsheng <kacysheng@mail.com>
2021-06-16 22:41:37 +1000

    Practice method chaining

--- js/script.js
+++ js/script.js
@@ -1,20 +1,12 @@
-// jquery really excels in traversing dom. the syntax is much simpler and light weight than vanilla javascript
-// next element
-$("img[alt=map").next().css({ border: "5px solid red" });
-$("#clients img").next().css({ border: "3px solid blue" });
-
-// previous element
-$("header nav").prev().css({ border: "3px solid yellow" });
-
-// parent, you can also use parents()
-
-$("#points-of-sale h3").parent().css({ border: "3px solid pink" });
-
-// children
-$("#contact-methods").children().css({ backgroundColor: "cyan" });
-
-// find
-$("#social-nav").find(".linked-in").css({ border: "1px solid black" });
-
-// closest parent - only looks up, not down
-$("header ul").closest("nav").css({ border: "1px solid gray" });
+// Method chaining speeds up code productivity
+$("#lead-banner p").css({ color: "red" }).next;
+$("#contact-methods")
+  .append("<p>")
+  .text("Just a new line of contact")
+  .css({ color: "red", fontSize: "30px" })
+  .next()
+  .css({ border: "3px solid blue" })
+  .closest("section")
+  .css({ border: "5px solid purple" })
+  .prev()
+  .css({ border: "5px solid green" });
