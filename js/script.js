349562badf10cc8deec585bf92b422ca4b04d3be
kcsheng <kacysheng@mail.com>
2021-06-16 20:58:01 +1000

    Practice jquery filters

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-// grabbing elements
-$(".wrapper p").css({ color: "blue" });
-$("h3").css({ border: "3px solid red" });
-$("#lead-banner").css({ border: "3px solid green" });
+// jquery filters - very much like css pseudoclasses - refining selectors, always starts with colon
+
+// :even and :odd are based on index, as jquery wraps elements in array.
+$("header nav li:even").css({ border: "2px solid blue" });
+$("header nav li:odd").css({ border: "2px solid yellow" });
+
+$("#contact ul:first").css({ backgroundColor: "yellow" });
+$("#contact ul:last").css({ border: "1px solid red" });
+// using :not
+$("section:not('#lead-banner')").css({ border: "1px solid cyan" });
+
+//:lt, :gt less than or greater than, be mindful of using index
+$("#contact-methods li:lt(2)").css({ border: "1px solid blue" });
+$("#contact-methods li:gt(1)").css({ border: "1px solid red" });
+
+// Attribute filters: a tag with specific attribute
+$("div[id]").css({ backgroundColor: "pink" });
+$("img[src]").css({ border: "5px solid gray" });
+$("a[href]").css({ textDecoration: "underline" });
+
+// Attribute with specific value: a tag with specific attribute=value pairs
+$("section[id=clients]").css({ backgroundColor: "red" });
