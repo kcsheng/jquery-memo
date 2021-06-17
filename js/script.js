9eb1993313ec01d2aa6e77fb0044c044c09c0eda
kcsheng <kacysheng@mail.com>
2021-06-17 23:52:15 +1000

    Practice slide up down toggle

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-// hide can do shrink, move and fadeout all at once.
-// show does the reverse
-$("section h3").on("click", function () {
-  $(this).hide(1000).show(1000);
+// slide up slide down slide toggle
+
+$(".slideup-btn").on("click", function () {
+  $("lead-banner").slideUp(400);
 });
-// toggle fade in or out depending on its current state
-// Whenever there is a toggle you need to have another element as a switch. In the following example, map is the switch.
-$("img[alt=map]").on("click", function () {
-  $("section > h2").toggle(1000);
+$(".slidedown-btn").on("click", function () {
+  $("lead-banner").slideDown(400);
+});
+$(".slidetoggle-btn").on("click", function () {
+  $("lead-banner").slideToggle(400);
 });
