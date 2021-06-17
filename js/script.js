28130a86c56bb4d28a9b8a1dfd70786567e1d3da
kcsheng <kacysheng@mail.com>
2021-06-17 12:31:23 +1000

    Practice event helpers

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 // jquery simplifies syntax of event binding/unbinding through using helpers
 
+// helpers are less better performers
+
 // $(".wrapper li").hover(() => {
 //   this.css({ border: "1px solid red" });
 // });
-$("#lead-banner").click(() => {
-  alert("You clicked!");
-});
-
-$("header li").click(() => {
-  $(this).css({ border: "2px solid red" });
+$("#lead-banner").hover(() => {
+  alert("You hovered on me!");
 });
