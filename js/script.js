83eb82c3afbbc02486c2d0bb2dc5b4d4a2eca8d4
kcsheng <kacysheng@mail.com>
2021-06-17 18:01:14 +1000

    Explore hide, show and toggle

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-// Replace css or animate with fade methods
-// Params in parentheses
-// Chaning them can produce a flashing effect
-
-$("section > h2").on("click", function () {
-  $(this).fadeOut(2000).fadeIn(500);
-});
-
+// hide can do shrink, move and fadeout all at once.
+// show does the reverse
 $("section h3").on("click", function () {
-  $(this).fadeTo(1000, 0.6).fadeTo(200, 1);
+  $(this).hide(1000).show(1000);
+});
+// toggle fade in or out depending on its current state
+// Whenever there is a toggle you need to have another element as a switch. In the following example, map is the switch.
+$("img[alt=map]").on("click", function () {
+  $("section > h2").toggle(1000);
 });
